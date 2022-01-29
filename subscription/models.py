2480302380a0416e70bca8199e1193722b715c76
lonaxit <lonaxit@outlook.com>
2022-01-29 18:59:17 +0100

    Added a model

--- subscription/models.py
+++ subscription/models.py
@@ -12,3 +12,24 @@ class ProductCategory(models.Model):
     
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    category = models.ForeignKey(ProductCategory,on_delete=models.DO_NOTHING,related_name='product')
+    
+    def __str__(self):
+        return self.name
+
+# class Loan(models.Model):
+    
+#     title= models.CharField(max_length=50)
+#     storyline = models.CharField(max_length=200)
+#     platform = models.ForeignKey(StreamPlatform,on_delete=models.CASCADE,related_name='watchlist')
+#     active = models.BooleanField(default=True)
+#     avg_rating = models.FloatField(default=0)
+#     number_rating = models.IntegerField(default=0)
+#     created = models.DateTimeField(auto_now_add=True)
+    
+#     def __str__(self):
+#         return self.title
