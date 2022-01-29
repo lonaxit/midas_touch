2ea7724bbac155174cc7b4989949b1b2c8f4d962
lonaxit <lonaxit@outlook.com>
2022-01-29 18:57:59 +0100

    Installed apps

--- midas_backend/settings.py
+++ midas_backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    
+    'subscription.apps.SubscriptionConfig',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
