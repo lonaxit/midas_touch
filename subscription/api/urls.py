38bd6a674dff029fad719b7b5fd335e0aef38aee
lonaxit <lonaxit@outlook.com>
2022-01-30 21:12:57 +0100

    Added new endpoints and corected comments

--- subscription/api/urls.py
+++ subscription/api/urls.py
@@ -5,15 +5,22 @@ from subscription.api.views import *
 
 urlpatterns = [
 
-    # list and create
+    # product category endpoints
     path('product-category/',ProductCategoryList.as_view(), name='product-category'),
     
-    # product category detail
      path('productcategory/<int:pk>/',ProductCategoryDetail.as_view(), name='productcategory-detail'),
      
+    #  products endpoints
     path('products/',ProductList.as_view(), name='products'),
+    
+    path('product/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
+    
+    path('<int:pk>/products/', ProductByCategoryList.as_view(), name="product-list"),
      
     # path('<int:pk>/', WatchDetailAV.as_view(), name="movie-detail"),
+    
+     # get reviews for a particular watchlist
+    # path('<int:pk>/reviews/', ReviewList.as_view(), name="review-list"),
     
     # to test filters on watch list
     # path('list2/',WatchListGV.as_view(), name='list2'),
