549b259f2a77422d28e677d5814ea9e2713d4479
lonaxit <lonaxit@outlook.com>
2022-01-30 21:18:01 +0100

    Added generics views for products

--- subscription/api/views.py
+++ subscription/api/views.py
@@ -32,9 +32,44 @@ class ProductCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ProductCategory.objects.all()
     serializer_class = ProductCategorySerializer
 
-
 # products views
 class ProductList(generics.ListCreateAPIView):
     
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    # allow get, put and destroy methods
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # permission_classes = [IsReviewUserOrReadOnly]
+    
+    # throttle_classes =[UserRateThrottle,AnonRateThrottle]
+    
+    # using ScopedRateThrottle
+    # throttle_classes =[ScopedRateThrottle]
+    # throttle_scope = 'review-detail'
+    
+class ProductByCategoryList(generics.ListAPIView):
+    # ListCreateAPIView gives us both the get and post methods
+    # queryset = Review.objects.all()
+    serializer_class = ProductSerializer
+    # adding permission, comment out since its a list or get request
+    # permission_classes = [IsAuthenticated]
+    
+    # default throttle classes
+    # throttle_classes =[UserRateThrottle, AnonRateThrottle]
+    
+    # throttle_classes =[ReviewListThrottle, AnonRateThrottle]
+    
+    # filtering backend
+    # filter_backends = [DjangoFilterBackend]
+    
+    # pls mention the fields to be filtered against 
+    # filterset_fields = ['review_user__username', 'active']
+    
+    # over writing default queryset 
+    def get_queryset(self):
+        # get the product category pk
+        pk = self.kwargs['pk']
+        return Product.objects.filter(category=pk)
