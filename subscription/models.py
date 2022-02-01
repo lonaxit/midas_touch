9a03d9e603f5f5c2398f7780736555ab4900740e
lonaxit <lonaxit@outlook.com>
2022-02-01 10:39:15 +0100

    Created a loan model

--- subscription/models.py
+++ subscription/models.py
@@ -21,7 +21,28 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-# class Loan(models.Model):
+class Loan(models.Model):
+    # user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product,on_delete=models.DO_NOTHING,related_name='loan')
+    status = models.BooleanField(default=False)
+    reference = models.CharField(max_length=200)
+    applied_amount = models.DecimalField(max_digits=20,decimal_places=2)
+    approved_amount = models.DecimalField(max_digits=20,decimal_places=2)
+    monthly_deduction = models.DecimalField(max_digits=20,decimal_places=2)
+    net_pay = models.DecimalField(max_digits=20,decimal_places=2)
+    tenor = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(36)])
+    
+    # review_by = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    review_date = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.product.name + " loan"
+    
+    
+ 
+
     
 #     title= models.CharField(max_length=50)
 #     storyline = models.CharField(max_length=200)
