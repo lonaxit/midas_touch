1a59b43e18792b042c7f4d9bf2cb76065a1de758
Tivkaa Luper <lupertivkaa@gmail.com>
2022-02-24 18:05:28 +0100

    Registered models

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from subscription.models import *
 # Register your models here.
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+admin.site.register(Loan)
+admin.site.register(ConsolidatedLoan)
+admin.site.register(LoanDeduction)
