32f79b7879d994dca14e9a4789b60f9281d1ee16
lonaxit <lonaxit@outlook.com>
2022-02-25 20:35:09 +0100

    Removed comments

--- subscription/models.py
+++ subscription/models.py
@@ -31,10 +31,6 @@ class Loan(models.Model):
     monthly_deduction = models.DecimalField(max_digits=20,decimal_places=2,null=True,blank=True)
     net_pay = models.DecimalField(max_digits=20,decimal_places=2)
     tenor = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(36)])
-    # created_by = models.ForeignKey(User,on_delete=models.DO_NOTHING)
-    # review_by = models.ForeignKey(User,on_delete=models.DO_NOTHING)
-    
-    # review_date = models.DateTimeField(auto_now_add=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
